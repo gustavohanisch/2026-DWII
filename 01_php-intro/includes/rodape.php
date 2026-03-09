df4b4dfe2c7786909f7efcbcbf4850cc4fd14b4f
gustavohanisch <20241ctb0100025@estudantes.ifpr.edu.br>
2026-03-09 19:29:36 +0000

    feat: aula03 -introducao ao PHP, variaveis e includes

---
+++ 01_php-intro/includes/rodape.php
@@ -0,0 +1,5 @@
+<footer style="background: #010000; color: 6b7280; text-aling: center;
+padding: 10px; margin-top: 5px; font-family: Arial, sans-serif; font-size: 14px;">
+<?php echo $nome; ?> &copy; <?php echo date("2026"); ?>
+|   Desenvolvido com PHP
+</footer>
